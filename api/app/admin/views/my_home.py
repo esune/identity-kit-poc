86b4d30767fade4d0e4ef3baf7329a25783a79fc
Emiliano Suñé <emiliano.sune@gmail.com>
2020-05-11 16:54:29 -0700

    Use UserSession when checking roles
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- api/app/admin/views/my_home.py
+++ api/app/admin/views/my_home.py
@@ -1,5 +1,6 @@
 from flask import redirect, session, url_for
 from flask_admin import AdminIndexView, expose
+from flask_pyoidc.user_session import UserSession
 
 from app import auth
 from config import Config
@@ -9,9 +10,12 @@ class MyHomeView(AdminIndexView):
     @expose("/")
     @auth.oidc_auth("default")
     def index(self):
-        id_token = session["id_token"]
+        user_session = UserSession(session)
         client_name = Config.OIDC_CLIENT
-        if client_name in id_token and "admin" in id_token[client_name]["roles"]:
+        if (
+            client_name in user_session.id_token
+            and "admin" in user_session.id_token[client_name]["roles"]
+        ):
             return super(MyHomeView, self).index()
         else:
-            return redirect(url_for("auth.unauthorized"))
+            return redirect(url_for("unauthorized"))
